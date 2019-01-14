06cb1fbc4a8bbe1dadd5d37d2f3039be4bb15339
Erin <erin_maas@yahoo.com>
2019-01-13 22:30:21 -0500

    Added writing and directing page, added JS for writing.html carousel

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,39 @@
 const hello = 'Hi World';
 console.log(hello);
 
+carousel = (function () {
+    let box = document.querySelector('.carouselbox');
+    let next = box.querySelector('.next');
+    let prev = box.querySelector('.prev');
+    let items = box.querySelectorAll('.content li');
+    let counter = 0;
+    let amount = items.length;
+    let current = items[0];
+    box.classList.add('active');
+    console.log('active added to box');
+
+    function navigate(direction) {
+        current.classList.remove('current');
+        console.log('current removed from li');
+
+        counter = counter + direction;
+
+        if (direction === -1 &&
+            counter < 0) {
+            counter = amount - 1;
+        }
+        if (direction === 1 &&
+            !items[counter]) {
+            counter = 0;
+        }
+        current = items[counter];
+        current.classList.add('current');
+    }
+    next.addEventListener('click', function (ev) {
+        navigate(1);
+    });
+    prev.addEventListener('click', function (ev) {
+        navigate(-1);
+    });
+    navigate(0);
+})();
