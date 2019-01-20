be039be981452e93fdbb00ec13dc950fc9cdf01c
Erin <erin_maas@yahoo.com>
2019-01-20 15:26:24 -0500

    working on writing page functionality

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,21 +1,25 @@
 const hello = 'Hi World';
 console.log(hello);
 
+// function clearBox(elementID) {
+//     document.getElementById(elementID).innerHTML = "";
+// };
 
 $(function () {
     $('.carousel1-pic').click(function () {
-        $('.carousel1-text').fadeIn('.hide');
+        // $('.text-container').html("");
+        $('.carousel1').fadeIn('.hide');
     });
     $('.carousel2-pic').click(function () {
-        $('.carousel1-text').fadeOut('.hide');
-        $('.carousel2-text').fadeIn('.hide');
+        $('.text-container').html("");
+        $('.carousel2').fadeIn('.hide');
     });
     $('.carousel3-pic').click(function () {
-        $('.carousel2-text').fadeOut('.hide');
-        $('.carousel3-text').fadeIn('.hide');
+        $('.text-container').html("");
+        $('.carousel3').fadeIn('.hide');
     });
     $('.carousel4-pic').click(function () {
-        $('.carousel3-text').fadeOut('.hide');
-        $('.carousel4-text').fadeIn('.hide');
+        $('.text-container').html("");
+        $('.carousel4').fadeIn('.hide');
     });
 });
