e05ec9dd0fa444c054a9a7e87d7d8b1b10df71a6
Erin <erin_maas@yahoo.com>
2019-01-20 22:07:06 -0500

    styling for writing page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,25 +1,43 @@
 const hello = 'Hi World';
 console.log(hello);
 
-// function clearBox(elementID) {
-//     document.getElementById(elementID).innerHTML = "";
-// };
+// Steps:
+// 1. click on the Image
+// 2. text appears below the Image
+// 3. click on a different Image
+// 4. text from previous click disappears 
+// 5. new text appears
+
+
 
 $(function () {
     $('.carousel1-pic').click(function () {
-        // $('.text-container').html("");
-        $('.carousel1').fadeIn('.hide');
+        $('.carousel0').hide();
+        $('.carousel2').hide();
+        $('.carousel3').hide();
+        $('.carousel4').hide();
+
+        $('.carousel1').show();
     });
     $('.carousel2-pic').click(function () {
-        $('.text-container').html("");
-        $('.carousel2').fadeIn('.hide');
+        $('.carousel1').hide();
+        $('.carousel3').hide();
+        $('.carousel4').hide();
+
+        $('.carousel2').show();
     });
     $('.carousel3-pic').click(function () {
-        $('.text-container').html("");
-        $('.carousel3').fadeIn('.hide');
+        $('.carousel1').hide();
+        $('.carousel2').hide();
+        $('.carousel4').hide();
+
+        $('.carousel3').show();
     });
     $('.carousel4-pic').click(function () {
-        $('.text-container').html("");
-        $('.carousel4').fadeIn('.hide');
+        $('.carousel1').hide();
+        $('.carousel2').hide();
+        $('.carousel3').hide();
+
+        $('.carousel4').show();
     });
 });
